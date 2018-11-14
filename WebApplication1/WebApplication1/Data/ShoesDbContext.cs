2d739fd5a34196f8788a6a4af3f06e4afa4b3e62
suzanne su <suhysu@gmail.com>
2018-11-14 08:36:24 -0800

    db creation

---
+++ WebApplication1/WebApplication1/Data/ShoesDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PumpedUpKicks.Data
+{
+    public class ShoesDbContext : DbContext
+    {
+        public ShoesDbContext(DbContextOptions<ShoesDbContext> options) : base(options)
+        {
+        }
+
+        // For composite keys and shadow properties
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+
+}
