03cd02ac06a355353485b0811e0ee4a5b42ce35d
earnest cain <ercain3@gmail.com>
2018-11-17 17:08:03 -0800

    fixed connection strings

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -37,10 +37,10 @@ namespace WebApplication1
                 .AddDefaultTokenProviders();
 
             services.AddDbContext<ShoesDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("LocalShoeConnection")));
 
             services.AddDbContext<ApplicationDBContext>(options =>
-               options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection"))); 
+               options.UseSqlServer(Configuration.GetConnectionString("LocalIdentityConnection"))); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
