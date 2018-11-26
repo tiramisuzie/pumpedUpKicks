6b8f2a8ba45fd082f9d83253c9e32923eade340f
suzanne su <suhysu@gmail.com>
2018-11-25 21:27:10 -0800

    add link to shopping cart on home page

--- WebApplication1/WebApplication1/Controllers/ShoppingCartController.cs
+++ WebApplication1/WebApplication1/Controllers/ShoppingCartController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PumpedUpKicks.Models;
@@ -9,6 +10,7 @@ using PumpedUpKicks.Models.Interfaces;
 
 namespace PumpedUpKicks.Controllers
 {
+    [Authorize]
     public class ShoppingCartController : Controller
     {
         private readonly IShoppingCart _shoppingCart;
@@ -19,7 +21,7 @@ namespace PumpedUpKicks.Controllers
             _shoppingCart = context;
             _userManager = userManager;
         }
-
+        
         public async Task<IActionResult> Index()
         {
             var user = await _userManager.GetUserAsync(HttpContext.User);
